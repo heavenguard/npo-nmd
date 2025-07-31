0da8db3aff292c94773360c27e2695685a08095a
heavenguard <heavenguard237@gmail.com>
2025-07-31 16:30:34 +0000

    Created correct groups so the layouts of the website and dashboard are different

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "./globals.css"
+import "../globals.css"
 import { Navigation } from "@/components/navigation"
 import { Footer } from "@/components/footer"
 
